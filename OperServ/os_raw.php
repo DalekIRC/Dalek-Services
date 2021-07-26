e31fd7a9e57e64e4eb0917ebef8d0a748d213f49
ValwareIRC <v.a.pond@outlook.com>
2021-07-26 16:46:24 +0100

    Create os_raw.php

---
+++ OperServ/os_raw.php
@@ -0,0 +1,58 @@
+<?php
+
+/*				
+//	(C) 2021 DalekIRC Services
+\\				
+//			dalek.services
+\\				
+//	GNU GENERAL PUBLIC LICENSE
+\\				v3
+//				
+\\				
+//				
+\\	Title: RAW
+//	
+\\	Desc: Raw commands lmao
+//	
+\\	
+//	
+\\	
+//	
+\\	Version: 1
+//				
+\\	Author:	Valware
+//				
+*/
+
+
+operserv::func("privmsg", function($u){
+
+	global $os;
+	$nick = new User($u['nick']);
+	if (!$nick->IsUser)
+	{
+		return;
+	}
+	
+	if (!$nick->account)
+	{
+		$os->notice($nick->uid,"Access denied!");
+		return;
+	}
+
+	if ($nick->account !== $operserv['oper'])
+	{
+		$os->notice($nick->uid,"Access denied!");
+		return;
+	}
+	
+	
+	
+	$parv = explode(" ",$u['msg']);
+	
+	if ($parv[0] !== "raw"){ return; }
+	
+	$raw = str_replace($parv[0]." ","",$u['msg']);
+	
+	$os->sendraw($raw);
+});
