177aab6905fbf2fe10ded4bb60ae9fd41a1a4d25
ValwareIRC <v.a.pond@outlook.com>
2021-07-26 18:24:35 +0100

    Update ns_regain.php

--- NickServ/ns_regain.php
+++ NickServ/ns_regain.php
@@ -25,24 +25,47 @@
 */
 
 
-nickserv::func("privmsg", function($u){
+nickserv::func("privmsg", function($u)
+{
 	
 	global $ns,$servertime,$cf;
 	
 	$parv = explode(" ",$u['msg']);
 	
-	if (!($nick = new User($u['nick']))->IsUser){ return; }
-	if ($parv[0] !== "recover" && $parv[0] !== "regain") { return; }
+	if (!($nick = new User($u['nick']))->IsUser)
+	{
+		return;
+		}
+	if ($parv[0] !== "recover" && $parv[0] !== "regain")
+	{
+		return;
+	}
 	
 	$account = (isset($parv[1])) ? $parv[1] : NULL;
 	$password = (isset($parv[2])) ? $parv[2] : NULL;
 	
 	/* TO DO: Make better response for incorrect parameters */
-	if (!$account || !$password){ $ns->notice($nick->uid,"Incorrect parameters."); return; }
+	if (!$account || !$password)
+	{
+		$ns->notice($nick->uid,"Incorrect parameters.");
+		return;
+	}
 	
-	if (!($nickToRegain = new User($account))->IsUser){ $ns->notice($nick->uid,IRC("ERR_NICKNOTONLINE")); return; }
-	
-	if (!df_verify_userpass($account,$password)){ $ns->notice($nick->uid,IRC("MSG_IDENTFAIL")); return; }
+	if (!($nickToRegain = new User($account))->IsUser)
+	{
+		$ns->notice($nick->uid,IRC("ERR_NICKNOTONLINE"));
+		return;
+	}
+	if ($nickToRegain->uid == $nick->uid)
+	{
+		$ns->notice($nick->uid,"You are already using that nick.");
+		return;
+	}
+	if (!df_verify_userpass($account,$password))
+	{
+		$ns->notice($nick->uid,IRC("MSG_IDENTFAIL"));
+		return;
+	}
 	
 	$ns->log($nickToRegain->nick." (".$nickToRegain->uid.") ".IRC("LOG_REGAIN")." ".$nick->nick." (".$nick->uid.")");
 	
@@ -56,7 +79,8 @@ nickserv::func("privmsg", function($u){
 	$ns->svs2mode($nick->uid,"+r");
 	$ns->notice($nick->uid,"$account ".IRC("MSG_REGAIN"));
 });
-nickserv::func("helplist", function($u){
+nickserv::func("helplist", function($u)
+{
 	
 	global $ns;
 	
