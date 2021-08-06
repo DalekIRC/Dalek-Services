dbdbf1184d64a30ef44efcfb61c33a6709488ac0
ValwareIRC <v.a.pond@outlook.com>
2021-08-06 21:24:40 +0100

    chanserb lol

---
+++ ChanServ/cs_set.php
@@ -0,0 +1,61 @@
+<?php
+/*				
+//	(C) 2021 DalekIRC Services
+\\				
+//			pathweb.org
+\\				
+//	GNU GENERAL PUBLIC LICENSE
+\\				v3
+//				
+\\				
+//				
+\\	Title: Set
+//	
+\\	Desc: Base command for set options
+//	
+\\	
+//	
+\\	
+//	
+\\	Version: 1
+//				
+\\	Author:	Valware
+//				
+*/
+
+chanserv::func("privmsg", function($u){
+	
+	global $cs;
+	
+	$parv = explode(" ",$u['msg']);
+	$nick = $u['nick'];
+	
+	if ($parv[0] == "set"){
+		
+		if (!isset($parv[1])){
+			chanserv::run("setlist", array('nick' => $nick));
+		}
+		else { chanserv::run("setcmd", array('nick' => $nick, 'cmd' => $u['msg'])); }
+		
+	}
+});
+
+chanserv::func("helplist", function($u){
+	
+	global $cs;
+	
+	$nick = $u['nick'];
+	
+	$cs->notice($nick,"SET                 Set options for your channel.");
+});
+
+
+chanserv::func("help", function($u){
+	
+	global $cs;
+	
+	if ($u['key'] !== "set"){ return; }
+	if (!isset($u['string'])){ $u['string'] = NULL; }
+	$nick = $u['nick'];
+	chanserv::run("setlist", array('nick' => $nick));
+});
--- server.php
+++ server.php
@@ -24,10 +24,11 @@
 */
 global $cf,$sql,$sqlip,$sqluser,$sqlpass,$sqldb,$server,$port,$serv,$servertime,$svs,$ns,$cs;
 
+
 include "hook.php";
 include "dalek.conf";
 include "language.php";
-if ($cf['proto'] == 'unreal5'){ include "protocol/unreal5.php"; }
+include "protocol/".$cf['proto'].".php";
 include "sql.php";
 include "client.php";
 include "user.php";
@@ -38,7 +39,7 @@ include "OperServ/operserv.php";
 include "Global/global.php";
 include "HostServ/hostserv.php";
 include "MemoServ/memoserv.php";
-
+//include "plugins/PATHWEB/uplink.php";
 // Server config
 $server = $cf['uplink'];
 $port = $cf['port'];
@@ -54,71 +55,89 @@ $sqldb = $cf['sqldb'];
 
 start:
 
-for (;;){
+for (;;)
+{
 	
-	if (!isset($sql)){ $sql = new SQL($sqlip,$sqluser,$sqlpass,$sqldb); hook::run("preconnect", array()); }
+	if (!isset($sql))
+	{ 
+		$sql = new SQL($sqlip,$sqluser,$sqlpass,$sqldb); hook::run("preconnect", array());
+	}
 	
-	if (!isset($serv)){ $serv = new Server($server,$port,$mypass); }
-	while ($input = fgets($socket, 1000)) {
+	if (!isset($serv)){ $serv = new Server($server,$port,$mypass);
+	}
+	while ($input = fgets($socket, 1000))
+	{
 		$timeget = microtime(true);	
 		$timetok = explode(".",$timeget);
-		if ($servertime != $timetok){ $servertime = $timetok[0]; }
-		if (!$socket){ die(); }
+		if ($servertime != $timetok)
+			$servertime = $timetok[0];
+		
+		if (!$socket)
+			die();
 	
-		if ($cf['debugmode'] == "on") { echo $input."\n"; if ($ns){ /*$ns->msg("#dalek",$input);*/ } }
+		if ($cf['debugmode'] == "on")
+			echo $input."\n";
+	
 		flush();
 		
 		$strippem = ircstrip(str_replace('\n','',str_replace('\r','',$input)));
 		$splittem = explode(' ',$strippem);
 		
 		// If the server pings us
-		if ($splittem[0] == 'PING') { 
+		if ($splittem[0] == 'PING')
+			$serv->sendraw("PONG ".$splittem[1]); 	// Ping it back
 		
-			// Ping it back
-			$serv->sendraw("PONG ".$splittem[1]);
+		elseif ($splittem[0] == 'ERROR')
+		{
 			
-		}
-		elseif ($splittem[0] == 'ERROR') {
-			
-			if (strpos($input,'Throttled') !== false) {
+			if (strpos($input,'Throttled') !== false)
+			{
 				$serv->hear("Uh-oh, we've been throttled! Waiting 40 seconds and starting again.");
 				sleep(40);
 				$serv->shout("Reconnecting...");
 				goto start;
 			}
-			elseif (strpos($input,'Timeout') !== false) {
+			elseif (strpos($input,'Timeout') !== false)
+			{
 				$serv->hear("Hmmmm. It seems there was a problem. Please check dalek.conf");
 				die();
 			}
-			elseif (strpos($input,'brb lmoa') !== false) {
+			elseif (strpos($input,'brb lmoa') !== false)
+			{
 				$serv->hear("Looks like we've been asked to restart! Lets go! Pewpewpew!");
 				goto start;
 			}
-			else {
+			else
+			{
 				$serv->hear("Unknown exit issue! Waiting 40 seconds and restarting");
 				sleep(40);
 				goto start;
 			}
 		}
-		else {
+		else
+		{
 			
 			$tagmsg = NULL;
-			if ($splittem[0][0] == '@'){
+			if ($splittem[0][0] == '@')
+			{
 				$tagmsg = $splittem[0];
 				$strippem = ltrim(str_replace($tagmsg,"",$strippem)," ");
 				$splittem = explode(" ",$strippem);
 			}
-			if ($splittem[0] == "PASS"){
+			if ($splittem[0] == "PASS")
+			{
 				
 				$pass = mb_substr($splittem[1],1);
 				
-				if ($pass !== $cf['serverpassword']){ die("Passwords do not match."); }
+				if ($pass !== $cf['serverpassword'])
+					die("Passwords do not match.");
 				
 				hook::run("connect", array());
 			}
 			$action = $splittem[1];
 			
-			if ($action == "PRIVMSG"){
+			if ($action == "PRIVMSG")
+			{
 				
 				$nick = mb_substr($splittem[0],1);
 				$dest = $splittem[2];
@@ -133,7 +152,8 @@ for (;;){
 				);
 				update_last($nick);
 			}
-			if ($action == "TAGMSG"){
+			if ($action == "TAGMSG")
+			{
 
 				$nick = mb_substr($splittem[0],1);
 				$dest = $splittem[2];
@@ -143,24 +163,26 @@ for (;;){
 					"mtags" => $tagmsg)
 				);
 			}
-			else {
+			else
 				hook::run("raw", array('string' => $strippem));
-			}
+			
 			
 		}
 	}
 }
 
-function get_string_between($string,$start, $end){
-			$string = ' ' . $string;
-			 $ini = strpos($string, $start);
-			 if ($ini == 0) return '';
-			 $ini += strlen($start);
-			 $len = strpos($string, $end, $ini) - 			$ini;
-  			return substr($string, $ini, $len);
+function get_string_between($string,$start, $end)
+{
+	$string = ' ' . $string;
+	$ini = strpos($string, $start);
+	if ($ini == 0) return '';
+	$ini += strlen($start);
+	$len = strpos($string, $end, $ini) - $ini;
+	return substr($string, $ini, $len);
 }
 
-function ircstrip($string){
+function ircstrip($string)
+{
 
 	$_ircstrip = str_replace(array(
                 chr(10),
